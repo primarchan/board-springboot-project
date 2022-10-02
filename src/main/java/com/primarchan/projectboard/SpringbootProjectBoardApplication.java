b075838d5fbf32f6031b26010d93269ad1331f18
An T.G <primarchan@gmail.com>
2022-10-02 19:37:55 +0900

    #6 - 스프링부트 프로젝트 시작
    스프링부트 프로젝트 초기화
    들어간 초기화 기술
    * Spring Web
    * Actuator
    * DevTools
    * Lombok

---
+++ src/main/java/com/primarchan/projectboard/SpringbootProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.primarchan.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/primarchan/projectboard/SpringbootProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primarchan.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
