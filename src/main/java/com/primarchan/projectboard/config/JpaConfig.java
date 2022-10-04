ab411203ce3ae9b4ec6a106a649162f521756ba8
An T.G <primarchan@gmail.com>
2022-10-05 00:58:52 +0900

    #10 - Spring Data JPA 속성 및 설정
    * JPA Auditing 기능을 사용하기 위한 JpaConfig 추가
    * DB 접근 및 개발 편의를 위한 각종 부트 프로퍼티 적용
    * TODO : 프로퍼티에 노출되어 있는 패스워드 보안 처리 필요

---
+++ src/main/java/com/primarchan/projectboard/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.primarchan.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO: Spring Security 인증 기능 구현 시 수정 예정
+        return () -> Optional.of("primarchan");
+    }
+}
