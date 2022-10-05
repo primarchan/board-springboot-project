a43169cb9c62b31518782f2e48c0a7714f5513ee
An T.G <primarchan@gmail.com>
2022-10-06 03:34:18 +0900

    #1 - QueryDSL 지원을 이용하여 API 검색 옵션 추가
    `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/primarchan/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/primarchan/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.primarchan.projectboard.repository;
 
 import com.primarchan.projectboard.domain.ArticleComment;
+import com.primarchan.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true); // 전체 필드에 대한 검색 기능 차단 활성화
+        bindings.including(root.content, root.createdAt, root.createdBy); // 검색 가능한 필드 설정
+        bindings.bind(root.content).first((StringExpression::containsIgnoreCase));
+        bindings.bind(root.createdAt).first((DateTimeExpression::eq));
+        bindings.bind(root.createdBy).first((StringExpression::containsIgnoreCase));
+    }
+
 }
