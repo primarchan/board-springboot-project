c4417b421896dff9f61cb165b283b4c9d3fc4afc
An T.G <primarchan@gmail.com>
2022-10-06 04:06:47 +0900

    #18 - 게시글 관련 뷰 테스트 작성
    정의된 API 명세를 기준으로 작성

---
+++ src/main/java/com/primarchan/projectboard/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.primarchan.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
