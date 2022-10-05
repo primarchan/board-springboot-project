6f7059e27624a22ded0ae8c6820c4a8135c91042
An T.G <primarchan@gmail.com>
2022-10-06 00:42:51 +0900

    #1 - Data REST 설정
    게시글, 댓글의 json api 를 자동으로 RESTful 하게 생성하게끔 설정

--- src/main/java/com/primarchan/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/primarchan/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.primarchan.projectboard.repository;
 
 import com.primarchan.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
