22757df47ebf9fe871a003c76a7004b440f956c6
An T.G <primarchan@gmail.com>
2022-10-06 22:31:27 +0900

    #18 - 게시글 뷰 골격 작성
    컨트롤러, 뷰 테스트 한 번에 적용

--- src/main/java/com/primarchan/projectboard/controller/ArticleController.java
+++ src/main/java/com/primarchan/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.primarchan.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,5 +17,12 @@ public class ArticleController {
         map.addAttribute("articles", List.of());
 
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 실제 데이터로 업데이트 예정
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
--- src/test/java/com/primarchan/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/primarchan/projectboard/controller/ArticleControllerTest.java
@@ -34,7 +34,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[View][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -44,7 +43,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("/articles/detail"))
+                .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
