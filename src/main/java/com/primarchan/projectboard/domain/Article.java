931fae72691eb07f37a8bd1363df0dada92f1650
An T.G <primarchan@gmail.com>
2022-10-06 23:34:26 +0900

    #18 - QueryDSL 관련 코드 리팩토링 실시

--- src/main/java/com/primarchan/projectboard/domain/Article.java
+++ src/main/java/com/primarchan/projectboard/domain/Article.java
@@ -18,7 +18,7 @@ import java.util.Set;
         @Index(columnList = "createdBy")
 })
 @Entity
-public class Article extends AuditingFeilds {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/primarchan/projectboard/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.primarchan.projectboard.repository;
+
+import com.primarchan.projectboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
