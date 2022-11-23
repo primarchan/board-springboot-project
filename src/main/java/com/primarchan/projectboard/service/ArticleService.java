d6fa72c2cc321fe746d0dccc91c5a710788a3e20
An T.G <primarchan@gmail.com>
2022-11-24 06:47:08 +0900

    #35 - 인증 기능의 뷰 연결
    게시글 / 댓글의 등록, 수정, 삭제 버튼이 적절한 상황에서 노출되고, 숨겨지게끔 함
    헤더에도 인증정보에 따라 표현이 달라지게끔 수정
    로그인 하면 헤더에 인증 사용자 id 와 알맞는 버튼을 볼 수 있음

--- src/main/java/com/primarchan/projectboard/service/ArticleService.java
+++ src/main/java/com/primarchan/projectboard/service/ArticleService.java
@@ -65,7 +65,8 @@ public class ArticleService {
             Article article = articleRepository.getReferenceById(articleId);
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
 
-            if (article.getUserAccount().equals(userAccount)) {
+            if (article.getUserAccount().getUserId().equals(userAccount.getUserId())) {
+            // if (article.getUserAccount().equals(userAccount)) {
                 if (dto.title() != null) { article.setTitle(dto.title()); }
                 if (dto.content() != null) { article.setContent(dto.content()); }
                 article.setHashtag(dto.hashtag());
