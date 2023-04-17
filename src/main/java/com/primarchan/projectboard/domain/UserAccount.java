dd032070c5582bccec408695732a7136cd50446d
An T.G <primarchan@gmail.com>
2023-04-17 17:55:25 +0900

    #54-equals(), hashcode() 에서 필드 접근을 getter 변경

--- src/main/java/com/primarchan/projectboard/domain/UserAccount.java
+++ src/main/java/com/primarchan/projectboard/domain/UserAccount.java
@@ -44,13 +44,13 @@ public class UserAccount extends AuditingFields {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof UserAccount userAccount)) return false;
-        return userId != null && userId.equals(userAccount.userId);
+        if (!(o instanceof UserAccount that)) return false;
+        return this.getUserId() != null && this.getUserId().equals(that.getUserId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId);
+        return Objects.hash(this.getUserId());
     }
 
 }
